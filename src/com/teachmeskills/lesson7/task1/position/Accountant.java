22ba9e7e4d28d67be7d46d550f7a849b2c4f69a1
Alina-Ilkevich <alina.1qaz@gmail.com>
2024-01-29 20:37:56 +0300

    init commit

--- src/com/teachmeskills/lesson7/task1/position/Accountant.java
+++ src/com/teachmeskills/lesson7/task1/position/Accountant.java
@@ -1,7 +1,5 @@
 package com.teachmeskills.lesson7.task1;
-
 import com.teachmeskills.lesson7.task1.getInfo.IGetInfo;
-
 public class Accountant implements IGetInfo {
     //This class is used to describe an accountant and
     //describe methods for displaying information about the accountant
--- src/com/teachmeskills/lesson7/task1/position/Director.java
+++ src/com/teachmeskills/lesson7/task1/position/Director.java
@@ -1,7 +1,5 @@
 package com.teachmeskills.lesson7.task1;
-
 import com.teachmeskills.lesson7.task1.getInfo.IGetInfo;
-
 public class Director implements IGetInfo {
     //This class is used to describe a director and
     //describe methods for displaying information about the director
